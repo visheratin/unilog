2dc066713de6eb7b76617567c34e857672b1fdbd
Alexander Visheratin <alexvish91@gmail.com>
2021-04-12 19:42:37 -0400

    Latest updates.

--- log.go
+++ log.go
@@ -5,24 +5,21 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func DefaultConfig() zap.Config{
+func DefaultConfig() zap.Config {
 	return zap.Config{
-		Encoding:         "console",
+		Encoding:         "json",
+		Development:      false,
 		Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
 		OutputPaths:      []string{"stdout"},
 		ErrorOutputPaths: []string{"stderr"},
 		EncoderConfig: zapcore.EncoderConfig{
-			MessageKey: "message",
-
-			LevelKey:    "level",
-			EncodeLevel: zapcore.CapitalLevelEncoder,
-
-			TimeKey:    "time",
-			EncodeTime: zapcore.ISO8601TimeEncoder,
-
-			CallerKey:    "caller",
-			EncodeCaller: zapcore.ShortCallerEncoder,
-
+			MessageKey:    "message",
+			LevelKey:      "level",
+			EncodeLevel:   zapcore.CapitalLevelEncoder,
+			TimeKey:       "time",
+			EncodeTime:    zapcore.ISO8601TimeEncoder,
+			CallerKey:     "caller",
+			EncodeCaller:  zapcore.ShortCallerEncoder,
 			StacktraceKey: "stacktrace",
 		},
 	}
